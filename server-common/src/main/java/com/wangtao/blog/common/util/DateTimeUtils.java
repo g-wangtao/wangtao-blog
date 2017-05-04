91b8682738372fafbfde65f4de1215c2616c97cd
g-wangtao <wangtao1127@live.com>
2017-05-04 09:46:55 +0800

    代码优化

--- server-common/src/main/java/com/wangtao/blog/common/util/DateTimeUtils.java
+++ server-common/src/main/java/com/wangtao/blog/common/util/DateTimeUtils.java
@@ -1,82 +1,144 @@
 package com.wangtao.blog.common.util;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
- * 
- * @author kalr
- *
+ * @ClassName:DateTimeUtils
+ * @author: KarlWang
+ * @Description: TODO(JDK1.8日期时间工具类) 
+ * @date:2017年4月24日 下午2:58:19
+ * @see com.wangtao.blog.common.util.DateTimeUtil
  */
-public final class DateTimeUtils {
+public class DateTimeUtils {
 
-    // 返回分钟数
-    public static final Character DATE_M = 'M';
-    // 返回小时数
-    public static final Character DATE_H = 'H';
-    // 返回天数
-    public static final Character DATE_D = 'D';
-    // 返回时分秒
-    public static final String TIME_FORMAT = "HH:mm:ss";
-    // 返回年月日
-    public static final String DATE_SHORT_FORMAT = "yyyyMMdd";
-    // 返回年月日
-    public static final String DATE_STAMP_FORMAT = "yyyy-MM-dd";
-    // 中文日期格式
-    public static final String DATE_CH_FORMAT = "yyyy年MM月dd日";
-    // 返回年月日时分秒
-    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    // 返回年月日时分秒毫秒
-    public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss.S";
-    // 一天开始时间
-    public static final String DAYTIME_START = "00:00:00";
-    // 一天结束时间
-    public static final String DAYTIME_END = "23:59:59";
-
-    // 指定格式日期处理对象
-    public  static SimpleDateFormat formatYmd = new SimpleDateFormat(DATE_STAMP_FORMAT);
-
-    // 指定格式日期处理对象
-    public static SimpleDateFormat formatYmdHms = new SimpleDateFormat(DATE_TIME_FORMAT);
-
-    // 加锁安全日期对象
-    private static ThreadLocal<SimpleDateFormat> local = new ThreadLocal<SimpleDateFormat>();
+    // 默认时间格式
+    private static final DateTimeFormatter DEFAULT_DATETIME_FORMATTER = DateTimeFormat.LONG_DATE_PATTERN_LINE.formatter;
 
     /**
-     * @see format date
-     * @author Barlow
-     * @param date
-     * @param formatPattern
-     * @return String
+     * 构造函数私有化
      */
-    public static String formatDate(Date date, String formatPattern) {
-        SimpleDateFormat formatter = local.get();
-        if (null == formatter){
-            formatter = new SimpleDateFormat(formatPattern);
-        }
-        return formatter.format(date);
+    private DateTimeUtils() {}
+
+    /**
+     * String 转时间
+     * @param timeStr
+     * @return
+     */
+    public static LocalDateTime parseTime(String timeStr) {
+        return LocalDateTime.parse(timeStr, DEFAULT_DATETIME_FORMATTER);
     }
 
     /**
-     * @see get date from string
-     * @author Barlow
-     * @param dateStr
-     * @return Date
+     * String 转时间
+     * @param timeStr
+     * @param format  时间格式
+     * @return
      */
-    public static Date getDateFromString(String dateStr,String formatPattern) {
-        DateFormat formatter = local.get();
-        if (null == formatter) {
-            formatter = new SimpleDateFormat(formatPattern);
-        }
-        Date date;
-        try {
-            date  = formatter.parse(dateStr);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-        return date;
+    public static LocalDateTime parseTime(String timeStr, DateTimeFormat format) {
+        return LocalDateTime.parse(timeStr, format.formatter);
     }
 
+    /**
+     * 时间转 String
+     * @param time
+     * @return
+     */
+    public static String parseTime(LocalDateTime time) {
+        return DEFAULT_DATETIME_FORMATTER.format(time);
+    }
+
+    /**
+     * 时间转 String
+     * @param time
+     * @param format 时间格式
+     * @return
+     */
+    public static String parseTime(LocalDateTime time, DateTimeFormat format) {
+        return format.formatter.format(time);
+    }
+
+    /**
+     * 获取当前时间
+     * @return
+     */
+    public static String getCurrentDatetime() {
+        return DEFAULT_DATETIME_FORMATTER.format(LocalDateTime.now());
+    }
+
+    /**
+     * 获取当前时间
+     * @param format 时间格式
+     * @return
+     */
+    public static String getCurrentDatetime(DateTimeFormat format) {
+        return format.formatter.format(LocalDateTime.now());
+    }
+
+    /**
+     * 内部日期格式化枚举类
+     */
+    public enum DateTimeFormat {
+
+        // 返回分钟数
+        DATE_M("M"),
+
+        // 返回小时数
+        DATE_H("H"),
+
+        // 返回天数
+        DATE_D("D"),
+
+        // 返回时分秒
+        TIME_FORMAT("HH:mm:ss"),
+
+        /**
+         * 短日期格式
+         */
+        SHORT_DATE_PATTERN_LINE("yyyy-MM-dd"),
+
+        SHORT_DATE_PATTERN_SLASH("yyyy/MM/dd"),
+
+        SHORT_DATE_PATTERN_DOUBLE_SLASH("yyyy\\MM\\dd"),
+
+        SHORT_DATE_PATTERN_NONE("yyyyMMdd"),
+
+        /**
+         * 长日期格式
+         */
+        LONG_DATE_PATTERN_LINE("yyyy-MM-dd HH:mm:ss"),
+
+        LONG_DATE_PATTERN_SLASH("yyyy/MM/dd HH:mm:ss"),
+
+        LONG_DATE_PATTERN_DOUBLE_SLASH("yyyy\\MM\\dd HH:mm:ss"),
+
+        LONG_DATE_PATTERN_NONE("yyyyMMdd HH:mm:ss"),
+
+        /**
+         * 长日期时间格式 带毫秒
+         */
+        LONG_DATE_PATTERN_WITH_MILSEC_LINE("yyyy-MM-dd HH:mm:ss.SSS"),
+
+        LONG_DATE_PATTERN_WITH_MILSEC_SLASH("yyyy/MM/dd HH:mm:ss.SSS"),
+
+        LONG_DATE_PATTERN_WITH_MILSEC_DOUBLE_SLASH("yyyy\\MM\\dd HH:mm:ss.SSS"),
+
+        LONG_DATE_PATTERN_WITH_MILSEC_NONE("yyyyMMdd HH:mm:ss.SSS"),
+
+        // 一天开始时间
+        DAYTIME_START("00:00:00"),
+
+        // 一天结束时间
+        DAYTIME_END("23:59:59")
+        ;
+
+        /**
+         * 获取默认时间格式: yyyy-MM-dd HH:mm:ss
+         */
+        private transient DateTimeFormatter formatter;
+
+        DateTimeFormat(String pattern) {
+            formatter = DateTimeFormatter.ofPattern(pattern);
+        }
+    }
 }
