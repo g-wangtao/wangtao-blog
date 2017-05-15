b15572173f175699ca190c233494f4c6ddfcc121
g-wangtao <wangtao1127@live.com>
2017-05-15 20:37:53 +0800

    添加系统基础常量接口类

---
+++ server-common/src/main/java/com/wangtao/blog/common/constant/interfaces/ISystemBaseConstant.java
@@ -0,0 +1,15 @@
+package com.wangtao.blog.common.constant.interfaces;
+
+/**
+ * @ClassName:SystemBaseConstant
+ * @author: KarlWang
+ * @Description: TODO(系统基础常量) 
+ * @date:2017年5月4日 下午3:44:45
+ * @see com.wangtao.blog.common.constant.interfaces.SystemBaseConstant
+ */
+public interface ISystemBaseConstant {
+	/** 生效 */
+	public static final char ACTIVE = 'Y';
+	/** 未生效 */
+	public static final char INACTIVE = 'N';
+}
