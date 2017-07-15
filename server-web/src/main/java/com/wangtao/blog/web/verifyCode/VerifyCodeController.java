9cf05d63cf70e52739b7355a745a3074350791f0
Administrator <Administrator@windows10.microdone.cn>
2017-07-15 19:06:23 +0800

    代码优化

--- server-web/src/main/java/com/wangtao/blog/web/verifyCode/VerifyCodeController.java
+++ server-web/src/main/java/com/wangtao/blog/web/verifyCode/VerifyCodeController.java
@@ -138,19 +138,18 @@ public class VerifyCodeController {
 			}
 		}
 	}
+	
 	//使用方法
-		@RequestMapping("/create")
-		public void create(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		 		// 设置响应的类型格式为图片格式
-	            response.setContentType("image/jpeg");
-	            //禁止图像缓存。
-	            response.setHeader("Pragma", "no-cache");
-	            response.setHeader("Cache-Control", "no-cache");
-	            response.setDateHeader("Expires", 0);
-	            
-	            
-	            VerifyCodeUtil verifyCode = new VerifyCodeUtil(100,30,5,10);
-	            request.getSession().setAttribute("verifyCode", verifyCode.getCode());
-	            verifyCode.write(response.getOutputStream());
-	     }
+	@RequestMapping("/create")
+	public void create(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		// 设置响应的类型格式为图片格式
+        response.setContentType("image/jpeg");
+        //禁止图像缓存。
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        VerifyCodeUtil verifyCode = new VerifyCodeUtil(100,30,5,10);
+        request.getSession().setAttribute("verifyCode", verifyCode.getCode());
+        verifyCode.write(response.getOutputStream());	
+	}	
 }
